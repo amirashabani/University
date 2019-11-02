ec33d74d9ab42426aeb037dfed3160d0b6fff504
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-02 19:46:23 +0330

    Finished the grammar; needs to be checked

--- 3981/Compiler/Project/SONEC.py
+++ 3981/Compiler/Project/SONEC.py
@@ -1,15 +1,18 @@
 import sys
+import antlr4
 from SONECLexer import SONECLexer
 
 def main():
-    file_path = 'test.sonec'
+    file_path = 'example.sonec'
 
     if(len(sys.argv) >= 2):
         file_path = sys.argv[1]
 
-    with open(file_path, 'r') as input_file:
+    with open(file_path, 'r') as input_file, open('output.txt', 'w') as output_file:
         lexer = SONECLexer(input_file)
-        print(lexer)
+        stream = antlr4.CommonTokenStream(lexer)
+        print(stream.tokens)
 
 if __name__ == "__main__":
     main()
+
--- 3981/Compiler/Project/SONECLexer.py
+++ 3981/Compiler/Project/SONECLexer.py
@@ -8,15 +8,12 @@ import sys
 def serializedATN():
     with StringIO() as buf:
         buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\4")
-        buf.write("\25\b\1\4\2\t\2\4\3\t\3\3\2\3\2\7\2\n\n\2\f\2\16\2\r\13")
-        buf.write("\2\3\3\6\3\20\n\3\r\3\16\3\21\3\3\3\3\2\2\4\3\3\5\4\3")
-        buf.write("\2\5\4\2C\\c|\5\2\62;C\\c|\5\2\13\f\17\17\"\"\2\26\2\3")
-        buf.write("\3\2\2\2\2\5\3\2\2\2\3\7\3\2\2\2\5\17\3\2\2\2\7\13\t\2")
-        buf.write("\2\2\b\n\t\3\2\2\t\b\3\2\2\2\n\r\3\2\2\2\13\t\3\2\2\2")
-        buf.write("\13\f\3\2\2\2\f\4\3\2\2\2\r\13\3\2\2\2\16\20\t\4\2\2\17")
-        buf.write("\16\3\2\2\2\20\21\3\2\2\2\21\17\3\2\2\2\21\22\3\2\2\2")
-        buf.write("\22\23\3\2\2\2\23\24\b\3\2\2\24\6\3\2\2\2\5\2\13\21\3")
-        buf.write("\b\2\2")
+        buf.write("\20\b\1\4\2\t\2\4\3\t\3\3\2\3\2\3\3\6\3\13\n\3\r\3\16")
+        buf.write("\3\f\3\3\3\3\2\2\4\3\3\5\4\3\2\3\5\2\13\f\17\17\"\"\2")
+        buf.write("\20\2\3\3\2\2\2\2\5\3\2\2\2\3\7\3\2\2\2\5\n\3\2\2\2\7")
+        buf.write("\b\7c\2\2\b\4\3\2\2\2\t\13\t\2\2\2\n\t\3\2\2\2\13\f\3")
+        buf.write("\2\2\2\f\n\3\2\2\2\f\r\3\2\2\2\r\16\3\2\2\2\16\17\b\3")
+        buf.write("\2\2\17\6\3\2\2\2\4\2\f\3\b\2\2")
         return buf.getvalue()
 
 
@@ -26,7 +23,7 @@ class SONECLexer(Lexer):
 
     decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
 
-    ID = 1
+    A = 1
     WS = 2
 
     channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
@@ -34,12 +31,12 @@ class SONECLexer(Lexer):
     modeNames = [ "DEFAULT_MODE" ]
 
     literalNames = [ "<INVALID>",
- ]
+            "'a'" ]
 
     symbolicNames = [ "<INVALID>",
-            "ID", "WS" ]
+            "A", "WS" ]
 
-    ruleNames = [ "ID", "WS" ]
+    ruleNames = [ "A", "WS" ]
 
     grammarFileName = "SONEC.g4"
 
