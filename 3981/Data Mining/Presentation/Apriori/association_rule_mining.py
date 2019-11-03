f25574af4553109b6fb64faac3e5f5bc65c17488
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-03 21:17:43 +0330

    Find association rules

--- 3981/Data Mining/Presentation/Apriori/association_rule_mining.py
+++ 3981/Data Mining/Presentation/Apriori/association_rule_mining.py
@@ -10,7 +10,11 @@ def main():
     records = []
     for row in range(row_count):
         records.append([x for x in data.iloc[row].tolist() if((type(x) != float) and (x != float('NaN')))])
-    print(records)
+
+    association_rules = apriori(records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
+    association_results = list(association_rules)
+
+    print(association_results)
 
 if __name__ == "__main__":
     main()
