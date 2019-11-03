ec5245907b4d06b92b83ce85a82d3a758b652eb6
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-03 21:55:02 +0330

    renamed to make it shorter

--- 3981/Data Mining/Presentation/Apriori/arm.py
+++ 3981/Data Mining/Presentation/Apriori/arm.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from apyori import apriori
 
+def pretty_rule(rule):
+    print(rule)
+
 def main(file_path):
     data = pd.read_csv("rawi3k.csv", header=None)
     (row_count, column_count) = data.shape
@@ -12,10 +15,10 @@ def main(file_path):
     for row in range(row_count):
         records.append([x for x in data.iloc[row].tolist() if((type(x) != float) and (x != float('NaN')))])
 
-    association_rules = apriori(records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
-    association_results = list(association_rules)
+    association_rules = list(apriori(records, min_support=0.0025, min_confidence=0.5, min_lift=3, min_length=2))
 
-    print(association_results)
+    for rule in association_rules:
+        pretty_rule(rule)
 
 if __name__ == "__main__":
     if(len(sys.argv) >= 2):
